f31479a78f229a7051519370e9214620b3cb8d63
Shubhamkar <shubhamkarayare@gmail.com>
2018-08-11 00:33:08 +0530

    assign4-p1-primes.py mostly done; usage not suppressed yet, and filename is displayed

---
+++ Assignment 4/P1/prime.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+# Reference: https://docs.python.org/3/library/argparse.html
+# https://docs.python.org/3/howto/argparse.html#id1
+
+import argparse
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--check_prime')
+parser.add_argument('--range', nargs=2)
+args = parser.parse_args()
+
+def checkPrime(n):
+    '''Returns True if n is Prime'''
+    n=int(n)
+    if (n==1):
+        return False
+#    print('n=',n)
+    sqrt_n = math.ceil(math.sqrt(n));
+    for i in range(2,sqrt_n+1):
+        if n%i==0:
+            return False
+    return True
+
+def nPrimesInRange(start,end):
+    '''
+    Return the number of primes from start to
+    end (both inclusive)
+    '''
+    count = 0
+    for i in range(start,end+1):
+        if checkPrime(i):
+            count += 1
+    return count
+            
+    
+arg_list = []
+
+
+
+if args.check_prime:
+    arg_list.append(int(args.check_prime))
+if args.range:
+    arg_list.append( int(parser.parse_args().range[0]))
+    arg_list.append( int(parser.parse_args().range[1]))
+if (not args.check_prime) and (not args.range):
+    parser.error('At least one of the following arguments are required: --check_prime, --range')
+
+
+for arg in arg_list:
+    if (arg < 1 or 1000 < arg):
+        parser.error('Please enter a value between 1 and 1000 only')
+    
+if args.check_prime:
+    if checkPrime(args.check_prime):
+        print('Yes',end=' ')
+    else:
+        print('No', end=' ')
+if args.range:
+    ini = int(parser.parse_args().range[0])
+    fin = int(parser.parse_args().range[1])
+    print(nPrimesInRange(ini,fin), end=' ')
+    
+print()
