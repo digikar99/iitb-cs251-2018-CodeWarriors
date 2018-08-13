9457a825248bd6fdcc34fbb284ce42906d734a77
Shubhamkar <shubhamkarayare@gmail.com>
2018-08-14 00:21:35 +0530

    assign4-p7,5,4-verified

--- Assignment 4/P7/use_inc.py
+++ Assignment 4/P7/use_inc.py
@@ -11,16 +11,17 @@ a2v = int(sys.argv[3])
 
 if a2v == 1:
     try:
-        ang = numpy.loadtxt(in_file, dtype='float', delimiter=',')
+        ang = numpy.loadtxt(in_file, delimiter=',')
         #print(ang)
         vec = inc.ang_to_vec(ang)
-        numpy.savetxt(out_file, vec, delimiter=',', fmt='%1.6f')
+        #print("Saving ",vec,"...")
+        numpy.savetxt(out_file, vec, delimiter=',')
     except:
         exit(1)
 elif a2v == 0:
     try:
-        vec = numpy.loadtxt(in_file, dtype='float', delimiter=',')
+        vec = numpy.loadtxt(in_file, delimiter=',')
         ang = inc.vec_to_ang(vec)
-        numpy.savetxt(out_file, ang, delimiter=',', fmt='%1.2f')
+        numpy.savetxt(out_file, ang, delimiter=',')
     except:
         exit(1)
