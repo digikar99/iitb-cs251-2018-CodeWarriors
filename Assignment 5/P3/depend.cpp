3bafd209091af4d2dcb3944bc5a7875d998e9cd9
sreyasr <42137090+sreyasr@users.noreply.github.com>
2018-08-19 14:31:15 +0530

    Add files via upload

---
+++ Assignment 5/P3/depend.cpp
@@ -0,0 +1,8 @@
+#include "depend.h"
+int factorial(int n){
+if(n!=1)
+{
+return(n * factorial(n-1));
+}
+else return 1;
+}
---
+++ Assignment 5/P3/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "depend.h"
+using namespace std;
+int main(){
+cout << "The factorial of 5 is " << factorial(5) << endl;
+return 0;
+}
