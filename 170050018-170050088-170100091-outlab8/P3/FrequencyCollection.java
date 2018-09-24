4cb602dba3ed82e5cadaf03db8665f4b8c12a135
sreyasr <42137090+sreyasr@users.noreply.github.com>
2018-09-24 22:36:39 +0530

    Add files via upload

--- 170050018-170050088-170100091-outlab8/P3/FrequencyCollection.java
+++ 170050018-170050088-170100091-outlab8/P3/FrequencyCollection.java
@@ -12,66 +12,62 @@ public class  FrequencyCollection
         HashMap<String, Integer> wordCountMap = new HashMap<String, Integer>();
 
         ArrayList<String> stop_words= new ArrayList<>(Arrays.asList("and","the","is","in","at","of","his","her","him"));
-
+     
         ArrayList<String> word_list= new ArrayList<>();
 
         BufferedReader reader = null;
          
         try
-        {
+        {    
             reader = new BufferedReader(new FileReader(args[0]));
-             
-            //Reading the first line into currentLine
              
             String currentLine = reader.readLine();
              
             while (currentLine != null)
             {    
-                //splitting the currentLine into words
                  
                 String[] words = currentLine.toLowerCase().split(" |\\t");
                  
-                //Iterating each word
-                 
                 for (String word : words)
                 {
-
-                	 if(word.isEmpty())
-	                 {
-	                    	continue;
-	                 }
-                    else if(!stop_words.contains(word))
+                    if(!stop_words.contains(word))
                     {
-                    	word_list.add(word);
+	                    if(word.isEmpty())
+	                    {
+	                    	continue;
+	                    }
+                        else
+                        {
+                            word_list.add(word);
+                        }
                     }
                 }
+
                 currentLine = reader.readLine();
             }
 
-            TreeMap<String,Integer> map = new TreeMap<>();
-
             for(String word : word_list)
-            {
-            	//if word is already present in map, updating its count
-            	if(map.containsKey(word))
+            {         
+                if(wordCountMap.containsKey(word))
                 {    
-                    map.put(word, map.get(word)+1);
+                    wordCountMap.put(word, wordCountMap.get(word)+1);
                 }
+                 
 
                 else
                 {
-                	map.put(word,1);
+                    wordCountMap.put(word, 1);
                 }
-
             }
+
+
+            TreeMap<String,Integer> map = new TreeMap<>(wordCountMap);
+
             map.forEach((k,v) -> System.out.println(k + "," + v));
-        }     
-         
-        catch (IOException e) 
-        {
-            e.printStackTrace();
+             
+             
         }
-        catch(NullPointerException e)
+        catch (IOException e) 
         {
             e.printStackTrace();
         }
@@ -86,5 +82,5 @@ public class  FrequencyCollection
                 e.printStackTrace();
             }
         }
-    }    
+    } 
 }
