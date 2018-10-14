b1d708e176caddd10a0377e200c8590b87a0f3e2
shubhamkar <shubhamkarayare@gmail.com>
2018-10-14 16:35:45 +0530

    outlab 10 - sql_injection done

---
+++ 170050018-170050088-170100091-outlab10/sql_injection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sqlite3
+import sys
+
+log_level = 0
+
+TABLE = {1:'TEAM', 2:'PLAYER', 3:'MATCH'}
+FNAME = {1: 'team_name', 2:'player_name', 3:'match_id'}
+table = TABLE[int(sys.argv[1])]
+field_name = FNAME[int(sys.argv[1])]
+
+isVulnerable = (True if (sys.argv[2]=='1') else False)
+if (log_level>0) and not (sys.argv[2]=='1' or sys.argv[1]=='0'):
+    print("Second argument is not 0 or 1:", sys.argv[2], file=sys.stderr)
+
+conn = sqlite3.connect('ipl.db')
+cur = conn.cursor()
+
+def executeVulnerably():
+    cur.execute("DELETE FROM " + table + " WHERE " + field_name
+                + " = '" + sys.argv[3] + "'")
+
+
+def executeWithCheck():
+    cur.execute("delete from " + table + " where " + field_name + " = (?)", (sys.argv[3],))
+
+executeVulnerably() if isVulnerable else executeWithCheck()
+
+conn.commit()
+conn.close()
