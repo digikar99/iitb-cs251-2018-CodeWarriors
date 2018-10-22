108d2d93d2305e7139deec99e052ca1566cf3491
Digikar99 <shubhamkarayare@gmail.com>
2018-10-23 01:10:22 +0530

    preparing the json object for uploading - only name, and content ready

--- project/linux-client.sh
+++ project/linux-client.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 # OUTLINE
+: '
+1. Authenticate
+2. Parse full file-path name, content, and may be, type to json 
+3. Upload the json object
+'
+
+####################################################################
 
 # 1. Authenticate
 # Reference: https://stackoverflow.com/questions/21306515/how-to-curl-an-authenticated-django-app
@@ -11,25 +18,39 @@ YOUR_PASS='1234'
 COOKIES=cookies.txt
 CURL_BIN="curl -s -c $COOKIES -b $COOKIES -e $LOGIN_URL"
 
-echo -n "Django Auth: get csrftoken ..."
+# echo -n "Django Auth: get csrftoken ..."
 $CURL_BIN $LOGIN_URL > /dev/null
 DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken\s*//')"
 
-echo -n " perform login ..."
+# echo -n " perform login ..."
 $CURL_BIN \
     -d "$DJANGO_TOKEN&username=$YOUR_USER&password=$YOUR_PASS" \
     -X POST $LOGIN_URL
 
-echo -n " do something while logged in ..."
+# echo -n " do something while logged in ..."
 $CURL_BIN \
     -d "$DJANGO_TOKEN&..." \
     -X POST https://yourdjangowebsite.com/whatever/
 
-# 2. Determine file type, get file contents, name and type in a json object
+##########################################################################
 
+# 2. Determine file type, get file contents, name and type in a json object
+FILE=$(realpath "$1")
+SPC_ROOT="/home/$USER/SPC/"
+# get file name wrt to the spc root
+NAME=$(echo ${FILE#$SPC_ROOT}) # is echo necessary?
+CONTENT=$(cat "$FILE")
+TYPE=""
+
+# Reference: https://stackoverflow.com/questions/12524437/output-json-from-bash-script
+JSON_OBJ="{\"name\":\""$NAME"\", \"content\":\""$CONTENT"\", \"type\":\""$TYPE"\"}"
+
+echo $JSON_OBJ
+
+########################################################################
 # 3. Send json object to the server
 
-echo " logout"
+# echo " logout"
 rm $COOKIES
 
 
