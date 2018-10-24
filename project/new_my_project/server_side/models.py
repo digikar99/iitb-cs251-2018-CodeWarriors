e71397fd2b35e180d16c2550ad804873882ea1f1
sreyasr <42137090+sreyasr@users.noreply.github.com>
2018-10-24 12:02:23 +0530

    Add files via upload

---
+++ project/new_my_project/server_side/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Directory(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, )
+    parent=models.IntegerField(blank=True)
+    name= models.TextField(blank=True)
+    path = models.TextField(blank=True)
+
+class File(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,)
+    parent = models.IntegerField(blank=True)
+    name = models.TextField(blank=True)
+    path = models.TextField(blank=True)
+    file_data=models.BinaryField(blank=True)
+
+# Although you might think about storing files in the database, consider that it is bad design in 99% of the cases.
+# This field is not a replacement for proper static files handling
