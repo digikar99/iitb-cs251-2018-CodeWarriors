499d524a8c4d9c6185089079b8eeaaaf968ed526
sreyasr <42137090+sreyasr@users.noreply.github.com>
2018-10-24 15:16:01 +0530

    Add files via upload

--- project/new_my_project/upload_file/views.py
+++ project/new_my_project/upload_file/views.py
@@ -10,9 +10,9 @@ def index(request):
         current_user=request.user
         flag="fail"
         if current_user.is_authenticated:
-        	flag="sucess"
-        	a=File(file_data=json_data['content'].encode())
-        return HttpResponse( str(type(json_data)) + 'The following data was recieved by POST:\n'+str(json_data)+'\n')
+            flag="sucess"
+            a=File(owner=current_user ,file_data=json_data['content'].encode())
+        return HttpResponse(flag + ' : The following data was recieved by POST:\n'+str(json_data)+'\n')
     response = HttpResponse('it was GET request')
     response.set_cookie('csrftoken', 'valid')
     return response
