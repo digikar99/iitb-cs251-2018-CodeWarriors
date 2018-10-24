389a1012cc7ff87cf1137e1e964b6f938e130e80
Digikar99 <shubhamkarayare@gmail.com>
2018-10-24 12:37:35 +0530

    spc_primitive - upload part remaining

--- project/spc_primitive/upload/views.py
+++ project/spc_primitive/upload/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
-def index(response):
-    return HttpResponse("File Uploaded")
+def upload(request):
+    return render(request, "home.html")
