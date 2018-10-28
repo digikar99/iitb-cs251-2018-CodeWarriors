39bada8f1350ef9b37d9f346d2b162e04e73b194
sreyasr <sreyasr7@gmail.com>
2018-10-28 21:02:04 +0530

    adding last-part of P3

---
+++ 170050018-170050088-170100091-outlab11/P2/scatter_quad.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
+import random
+import csv
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+def colorcode(x,y,z,a,b,c):
+	return a*z+b*(x*x+y*y)+c>=0
+
+a=1
+b=1
+c=1
+with open('3dpd.out') as f:
+	filereader=csv.reader(f)
+	for row in filereader:
+		x=float(row[0])
+		y=float(row[1])
+		z=float(row[2])
+		if colorcode(x,y,z,a,b,c):
+			ax.scatter(x,y,z,c='r')
+		else:
+			ax.scatter(x,y,z,c='b')
+
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
