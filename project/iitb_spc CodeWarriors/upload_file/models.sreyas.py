07dec5be920a1798d01a52033d5c6856c9537cc4
sreyasr <sreyasr7@gmail.com>
2018-11-22 22:11:34 +0530

    unique user file-name pair

--- project/iitb_spc CodeWarriors/upload_file/models.sreyas.py
+++ project/iitb_spc CodeWarriors/upload_file/models.sreyas.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Directory(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, )
-    parent=models.IntegerField(blank=True)
-    name= models.TextField(blank=True)
-    path = models.TextField(blank=True)
 
 class File(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE,)
-    parent = models.IntegerField(blank=True)
-    name = models.TextField(blank=True)
-    path = models.TextField(blank=True)
-    file_data=models.BinaryField(blank=True)
-    timestamp=models.TextField(blank=True)
-    file_type=models.TextField(blank=True)
-    part_no=models.IntegerField(blank=True)
-    md5sum=models.TextField(blank=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,null=True
+    )
+    name_path = models.TextField()
+    file_data=models.TextField(blank=True, null=True)
+    last_update_time = models.IntegerField()
+    # file_type=models.TextField(blank=True, null=True)
+    # part_no=models.IntegerField(blank=True, null=True) # implement for block level encryption
+    md5sum=models.TextField()
+
+    class Meta:
+    	unique_together = ('owner', 'name_path',)
+
+    def __str__(self):
+        r=(self.owner, self.name_path)
+        return str(r)
+
 # Although you might think about storing files in the database, consider that it is bad design in 99% of the cases.
 # This field is not a replacement for proper static files handling
