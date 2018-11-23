0f098c424df6fd23c5885e72fb1388e90a7d8c40
sreyasr <sreyasr7@gmail.com>
2018-11-24 03:31:45 +0530

    lock

--- project/iitb_spc CodeWarriors/upload_file/views.py
+++ project/iitb_spc CodeWarriors/upload_file/views.py
@@ -5,6 +5,8 @@ from django.views import generic
 from . import models
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseForbidden
+from download_file.models import UserSession
 
 class UploadFileForm(forms.ModelForm):
     class Meta:
@@ -19,8 +21,22 @@ class UploadFile(generic.CreateView):
     # def dispatch(self, *args, **kwargs):
     #     return super(UploadFile, self).dispatch(*args, **kwargs)
 
-    # def form_valid(self, form):
-    #     obj = form.save(commit=False)
-    #     obj.owner = self.request.user
-    #     obj.save()
-    #     return redirect(('/home'))
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        if obj.user_name_path.split('/')[0] == self.request.user.username:
+            Session = UserSession.create(self.request)
+            prev_Session = UserSession.objects.filter(username=self.request.user)
+            if not prev_Session:
+                Session.save()
+            else:
+                time_diff = Session.get_time() - prev_Session[0].get_time()
+                if time_diff.total_seconds() > 15 * 60 or Session.ip == prev_Session[0].ip:
+                    a=models.File.objects.filter(user_name_path = obj.user_name_path)
+                    a.delete()
+                else:
+                    return HttpResponseForbidden()
+            obj.save()
+        else:
+            return HttpResponseForbidden()
+        super(UploadFile, self).form_valid(form)
+        return redirect(('/download_file/sucess/'))
